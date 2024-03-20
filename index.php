8d837e417be1c4a1c07bca2833468f42413b0075
Marshddd <164147640+Marshddd@users.noreply.github.com>
2024-03-20 15:07:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Python</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>   
+            body {
+                  background-image: url('Python-programming-for-hackers-compressed.jpg');
+                  background-position: center;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+                  height: 100vh;
+                  width: 100%;
+                  margin: 0;
+                  padding: 0
+            }   
+            nav{
+                background:none!important ;
+                color: white!important;
+
+            }
+            .button {
+            display: inline-block;
+            padding: 12px 24px;
+            background-color: #4CAF50;
+            color: white;
+            text-align: center;
+            font-size: 16px;
+            cursor: pointer;
+            border: none;
+            border-radius: 8px;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+            margin: 0 auto;
+            display: block;
+        }
+            .cool-button {
+            display: inline-block;
+            padding: 10px 50px;
+            font-size: 16px;
+            color: #000;
+            background-color: #fff;
+            border: 2px solid transparent;
+            border-radius: 4px;
+            transition: all 0.3s ease;
+            text-decoration: none;
+            cursor: pointer;
+            text-align: center;
+            /* margin: 0 auto;
+            display: block;
+            text-align: center; */
+            
+        }
+        
+
+        </style>
+</head>
+  <body>
+        
+    <nav class="  nav navbar navbar-expand-lg text-light">
+      <div class="container-fluid">
+        <a class="navbar-brand text-dark  bg-body-tertiary" href="#" ></a>
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="">
+          <div class="navbar-nav">
+          <a class="navbar-brand text-light" href="#">
+      <img src="bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ">
+      AOI
+    </a>
+          <a class="nav-link active  bg-body-tertiary text-dark" aria-current="page" href="index.php" >หน้าหลัก</a>
+          <a class="nav-link active  bg-body-tertiary text-dark" aria-current="page" href="db.php" >เนื้อหาบทเรียน</a>
+          <a class="nav-link active  bg-body-tertiary text-dark" aria-current="page" href="pic.php">ข้อมูลทั่วไป</a>
+            <a class="nav-link text-light bg-body-tertiary text-dark" href="paisan.php">คณะที่ปรึกษา</a>
+            <a class="nav-link text-light bg-body-tertiary text-dark" href="name.php">คณะผู้จัดทำ</a>
+          </div>
+        </div>
+      </div>
+    </nav>  
+  <nav class="navbar ">
+  <div class="container-fluid">
+  
+
+     
+</div>
+
+</nav>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br>
+
+
+    <div style="text-align: center;">
+        <a href="db.php" class="cool-button " style="text-align: center; ">ไปต่อ</a>
+    </div>
+  </body>
+</html>
